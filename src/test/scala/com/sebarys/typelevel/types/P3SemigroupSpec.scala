6ebb73d12325f9d4492e2f81cda07ba7f8f3346e
Rysztun, Sebastian <sebastian.rysztun@gmail.com>
2018-09-17 23:12:45 +0200

    semigroup

---
+++ src/test/scala/com/sebarys/typelevel/types/P3SemigroupSpec.scala
@@ -0,0 +1,90 @@
+package com.sebarys.typelevel.types
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class P3SemigroupSpec extends FlatSpec with Matchers {
+
+  trait Semigroup[A] {
+    def combine(x: A, y: A): A
+  }
+
+  object Semigroup {
+    def apply[A: Semigroup]: Semigroup[A] = implicitly[Semigroup[A]]
+
+    implicit class SemigroupOps[A: Semigroup](x: A) {
+      def combine(y: A): A = Semigroup[A].combine(x, y)
+    }
+
+  }
+
+  implicit val intAdditionSemigroup = new Semigroup[Int] {
+    override def combine(x: Int, y: Int): Int = x + y
+  }
+
+  behavior of "semigroup"
+
+  it should "allow for combine ints when required instances are provided" in {
+
+    //given
+    import Semigroup._
+
+    //when
+    val res = 5.combine(6)
+
+    //then
+    res shouldEqual 11
+  }
+
+  /**
+    * Exercise 1: Implement Semigroup for Map[A,B]
+    */
+  implicit def mapValueAdditionSemigroup[A, B: Semigroup] = new Semigroup[Map[A, B]] {
+
+    import Semigroup._
+
+    //if maybeY is present combine it with x, otherwise return x
+    def optionCombine[B: Semigroup](x: B, maybeY: Option[B]): B = ???
+
+    // hint: we can accumulate results using foldLeft with initial value
+    override def combine(firstMap: Map[A, B], secondMap: Map[A, B]): Map[A, B] = ???
+  }
+
+  it should "allow for combine map when values type provide instance of semigroup" in {
+
+    //given
+    val map1 = Map("a" -> 6, "b" -> 7, "c" -> 10)
+    val map2 = Map("a" -> 4, "b" -> 3, "d" -> 10)
+    import Semigroup._
+
+    //when
+    val res = map1 combine map2
+    //then
+    res shouldEqual Map("a" -> 10, "b" -> 10, "c" -> 10, "d" -> 10)
+  }
+
+  /**
+    * Exercise 2: Implement Semigroup for our custom class - Order
+    */
+  case class Order(items: Map[String, Int])
+
+  implicit val orderAdditionSemigroup = new Semigroup[Order] {
+    override def combine(x: Order, y: Order): Order = ???
+  }
+
+  it should "allow for combine custom types and reuse existing combine implementations" in {
+
+    //given
+
+    import Semigroup._
+
+    val order1 = Order(Map("item1" -> 6, "item2" -> 7, "item3" -> 10, "item5" -> 2))
+    val order2 = Order(Map("item1" -> 4, "item3" -> 7))
+
+    //when
+    val res = order1 combine order2
+
+    //then
+    res shouldEqual Order(Map("item1" -> 10, "item2" -> 7, "item3" -> 17, "item5" -> 2))
+  }
+
+}
