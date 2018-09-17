315a173f40f1c83edc77aaffc1e471ced450fee4
Rysztun, Sebastian <sebastian.rysztun@gmail.com>
2018-09-17 23:08:28 +0200

    type constructors

---
+++ src/test/scala/com/sebarys/typelevel/types/P1TypesConstructorSpec.scala
@@ -0,0 +1,20 @@
+package com.sebarys.typelevel.types
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class P1TypesConstructorSpec extends FlatSpec with Matchers {
+  behavior of "types"
+
+  it should "allow to create proper types from type constructor (1st order kinded type)" in {
+
+    //we have generic type Maybe and by providing concrete type to type constructor we receive concrete kind of Maybe
+    //our generic type is just a context to which we provide our concrete type
+    
+    val justInt: Just[Int] = new Just(4)
+    val emptyInt: Empty[Int] = new Empty[Int]()
+
+    val justString: Just[String] = new Just("test")
+    val emptyString: Empty[String] = new Empty[String]()
+
+  }
+}
